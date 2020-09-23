34065efaeef739daf7b37aaf7d0021805bde0489
ZinajdaH <68381660+ZinajdaH@users.noreply.github.com>
2020-09-23 13:47:47 +0200

    Create Python.py

---
+++ Python.py
@@ -0,0 +1,15 @@
+class Foo:
+  def __init__(self):
+    pass
+
+class Bar:
+  def __init__(self):
+    pass
+    
+    class Poo:
+  def __init__(self):
+    pass
+
+class Dar:
+  def __init__(self):
+    pass
